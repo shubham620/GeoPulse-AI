0c8615a49a6d3fcba592a75245a4ad1c0db042e2
shubham raj <68488122+shubham620@users.noreply.github.com>
2026-03-12 15:29:30 +0530

    Update fetch_news function to use valid API URL

--- data_fetcher/fetch_news.py
+++ data_fetcher/fetch_news.py
@@ -1,10 +1,10 @@
 import requests
 
-API_KEY = "NEWS_API_KEY"
+API_KEY = "a4fbec0909a4442b911ef550476788dc"
 
 def fetch_news():
 
-    url = enter your url={API_KEY}"
+    url = f"https://newsapi.org/v2/everything?q=geopolitics&language=en&apiKey={API_KEY}"
 
     response = requests.get(url)
     data = response.json()
@@ -21,4 +21,5 @@ def fetch_news():
         })
 
     return articles
+
 
